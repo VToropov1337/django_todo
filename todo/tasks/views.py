453b766f76b6b2270b87a09c0077af0969dd20cd
VToropov1337 <vladimirtoropov87@gmail.com>
2020-01-07 19:43:48 +0300

    save

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -19,7 +19,6 @@ def index(request):
     return render(request, 'tasks/list.html', context)
 
 
-
 def updateTask(request, pk):
     task = Task.objects.get(id=pk)
     form = TaskForm(instance=task)
@@ -32,3 +31,13 @@ def updateTask(request, pk):
 
     context = {"form" : form}
     return render(request, 'tasks/update_task.html', context)
+
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {"item" : item}
+    return render(request, 'tasks/delete.html', context)
